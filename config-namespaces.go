91119ae40c6246037d41d1731ca14338b833aa74
xingw <xingw@mingyuanyun.com>
2022-06-19 12:56:09 +0800

    feat: 封装apollo config包

---
+++ config-namespaces.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"github.com/philchia/agollo/v4"
+)
+
+const (
+	NAMESPACE_APP = "application"
+)
+
+func setApolloNamespaces(conf *agollo.Conf) {
+	namespace := getApolloNamespace()
+	conf.NameSpaceNames = append(conf.NameSpaceNames, namespace)
+	currentNamespace = namespace
+}
+
+func getApolloNamespace() string {
+	apolloNamespace := NAMESPACE_APP
+
+	tenantNamespace := GetTenantNamespace()
+	if tenantNamespace == "" || tenantNamespace == "g2" {
+		return apolloNamespace
+	}
+
+	currentNamespace := tenantNamespace + "_" + NAMESPACE_APP
+	return currentNamespace
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/yunkeCN/apollo-golang
 
+require (
+	github.com/goinggo/mapstructure v0.0.0-20140717182941-194205d9b4a9
+	github.com/philchia/agollo/v4 v4.1.1
+)
+
 go 1.15
